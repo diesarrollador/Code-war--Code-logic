903e502fc90d0e4027b91b36390287d64f25f476
Diesarrollador <xdiegoxgomez@gmail.com>
2021-05-16 15:31:40 -0500

    Código: encuentre el factorial de un número dado

---
+++ season-2/FirstFactorial.py
@@ -0,0 +1,13 @@
+""" Haga que la función que tome el parámetro num 
+que se está pasando y devuelva el factorial del mismo.
+Por ejemplo: si num = 4,
+entonces su programa debería 
+devolver (4 * 3 * 2 * 1) = 24.
+Para los casos de prueba,
+el rango estará entre 1 y 18 y
+la entrada siempre será un número entero. """
+from math import factorial
+FirstFactorial = lambda num: factorial(num)
+
+
+print(FirstFactorial(13))
