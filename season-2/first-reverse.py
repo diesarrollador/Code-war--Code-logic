75d590d83df945a54c55dce9bd75b0acfb478189
Diesarrollador <xdiegoxgomez@gmail.com>
2021-05-16 16:13:27 -0500

    Código: devuelva la cadena en el orden inverso

---
+++ season-2/first-reverse.py
@@ -0,0 +1,6 @@
+""" Haga que la función que tome el parámetro str
+que se está pasando y devuelva la cadena en orden inverso.
+Por ejemplo: si la cadena de entrada es "Hello World and Coders",
+entonces su programa debe devolver la cadena sredoC dna dlroW olleH. """
+FirstReverse = lambda strParam: strParam[::-1]
+print(FirstReverse("Hello World and Coders"))
