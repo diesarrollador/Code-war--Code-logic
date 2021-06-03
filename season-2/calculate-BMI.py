88e6dbbdd6d55c0de0a2bc4aa1ddf2cd9bf27033
Diesarrollador <xdiegoxgomez@gmail.com>
2021-06-02 19:36:29 -0500

    Cambiando de una función normal a una lambda

--- season-2/calculate-BMI.py
+++ season-2/calculate-BMI.py
@@ -1,12 +1,7 @@
 """ Escriba la función bmi que calcula 
 el índice de masa corporal 
 (bmi = peso / altura2). """
-def bmi(weight, height):
-    BMI = round(weight / (height**2), 1)
-    if BMI <= 18.5 : return 'Underweight'
-    if BMI <= 25.0 : return 'Normal'
-    if BMI <= 30.0 : return 'Overweight'
-    return 'Obese'
+bmi = lambda w, h : 'Underweight' if round(w / (h**2), 1) <= 18.5 else 'Normal' if round(w / (h**2), 1) <= 25.0 else 'Overweight' if round(w / (h**2), 1) <= 30.0 else 'Obese'
 
 # TEST CASES
 print(bmi(50, 1.80))#Output Underweight
