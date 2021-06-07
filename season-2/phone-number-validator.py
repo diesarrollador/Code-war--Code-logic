e1b4507724d10d65887571f006ce3d783f0a1c96
Diesarrollador <xdiegoxgomez@gmail.com>
2021-06-07 16:40:58 -0500

    Haciendo correcciones

--- season-2/phone-number-validator.py
+++ season-2/phone-number-validator.py
@@ -4,7 +4,7 @@ Un número de teléfono válido tiene exactamente 8
 dígitos y comienza con 1, 8 o 9.
 Salida "Válido" si el número es válido y "Inválido" si no lo es. """
 import re
-phone_number_validator = lambda num : 'Valid' if len(num) == 8 and re.match(r'^(1|8|9)[0-9]', num) else 'Invalid'
+phone_number_validator = lambda num : 'Valid' if len(num) == 8 and re.match(r'^(1|8|9)[0-9]{7}', num) else 'Invalid'
 
 if __name__ == '__main__':
     print(phone_number_validator(num := input()))
