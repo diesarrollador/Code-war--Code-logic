ab22f1a434542d761feb6c91c7add98bc83d029b
Diesarrollador <xdiegoxgomez@gmail.com>
2021-06-09 21:28:02 -0500

    Código: calcule el mean, var, std

---
+++ season-2/mean-var-std.py
@@ -0,0 +1,51 @@
+import numpy
+"""
+https://www.hackerrank.com/challenges/np-mean-var-and-std/problem?h_r=next-challenge&h_v=zen
+Tarea:
+Se le da una matriz 2-D de tamaño NxM.
+Tu tarea es encontrar:
+La media a lo largo del axis 1
+La var a lo largo del axis 0
+El std a lo largo del axis None
+
+Formato de entrada:
+La primera línea contiene los valores 
+separados por espacios de N y M.
+Las siguientes N líneas contienen M enteros
+separados por espacios.
+
+Formato de salida:
+Primero, imprima el mean.
+En segundo lugar, imprima la var.
+En tercer lugar, imprima el std.
+
+"""
+def mean_var_std(data2):
+    ARRAY = numpy.array(data2)
+    for i in range(3):
+        if i == 0:
+            yield numpy.mean(ARRAY, axis = 1)
+        elif i == 1:
+            yield numpy.var(ARRAY, axis = 0)
+        elif i == 2:
+            yield round(numpy.std(ARRAY, axis = None), 11)
+
+if __name__ == '__main__':
+    lista = list()
+    filas, colum = list(map(int, input().split()))
+    for i in range(filas):
+        lista.append([])
+        lista[i].extend(list(map(int, input().split())))
+
+    for i in mean_var_std(lista):
+        print(i)
+
+# Test input:
+# 2 2
+# 1 2
+# 3 4
+
+# Output
+# [ 1.5  3.5]
+# [ 1.  1.]
+# 1.11803398875
