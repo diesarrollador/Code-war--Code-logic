39a3aaa37e712d5d53d9605071894731f19f708a
Diesarrollador <xdiegoxgomez@gmail.com>
2021-06-11 21:45:32 -0500

    Pasando la solución a una función lambda

--- season-2/occurrences-substring.py
+++ season-2/occurrences-substring.py
@@ -4,13 +4,7 @@ número de veces que ocurre la subcadena
 en la cadena dada. El recorrido de la
 cadena se realizará de izquierda a derecha,
 no de derecha a izquierda. """
-def count_substring(string, sub_string):
-    count, ver = int(), str()
-    for i in range(len(string)):
-        ver = string[i : len(sub_string) + i]
-        if ver == sub_string:
-            count += 1
-    return count
+count_substring = lambda s, sb: len([j for j in [1 if s[i : len(sb) + i] == sb else 2 for i in range(len(s))] if j == 1])
 
 if __name__ == '__main__':
     string = input().strip()
